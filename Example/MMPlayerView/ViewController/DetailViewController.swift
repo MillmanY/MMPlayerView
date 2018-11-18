3f0d85ccc47cfed96f54c49a31b7ba23cc6b86cf
Millman <millmanyang@gmail.com>
2018-11-18 14:34:38 +0800

    no message

--- Example/MMPlayerView/ViewController/DetailViewController.swift
+++ Example/MMPlayerView/ViewController/DetailViewController.swift
@@ -84,6 +84,9 @@ class DetailViewController: UIViewController {
                     self?.progress.isHidden = true
                 }
             }
+            self.playerLayer?.download(observer: { (status) in
+                
+            })
         } else {
             let alert = UIAlertController(title: "download only for ios 11", message: "", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
